8be104379c8a1834d4306cf7b1606ce89e495d5d
Paul Armstrong <paul@spaceyak.com>
2017-09-22 16:37:43 -0700

    Add tests, build command

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,38 @@
+/* eslint-env jest */
+import Benchmark from '../';
+import BenchmarkType from '../BenchmarkType';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+class Test extends React.Component {
+  render() {
+    return <div id="test" />;
+  }
+}
+
+describe('Benchmark', () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      component: Test,
+      onComplete: jest.fn(),
+      samples: 2
+    };
+  });
+
+  it('renders nothing if not running', () => {
+    const component = shallow(<Benchmark {...props} />);
+    expect(component.get(0)).toBeNull();
+  });
+
+  it('runs the given samples', () => {
+    const component = shallow(<Benchmark {...props} />);
+    component.instance().start();
+    expect(props.onComplete).toHaveBeenCalledWith(
+      expect.objectContaining({
+        sampleCount: 2
+      })
+    );
+  });
+});
