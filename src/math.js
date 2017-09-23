454e6f28a4a2b99dd310ae34e1330415aabf71b4
Paul Armstrong <paul@spaceyak.com>
2017-09-23 07:52:15 -0700

    flow math

--- src/math.js
+++ src/math.js
@@ -1,9 +1,10 @@
-type Values = Array<number>;
+// @flow
+type ValuesType = Array<number>;
 
-export const getStdDev = (values: Values): number => {
+export const getStdDev = (values: ValuesType): number => {
   const avg = getMean(values);
 
-  const squareDiffs = values.map(value => {
+  const squareDiffs = values.map((value: number) => {
     const diff = value - avg;
     return diff * diff;
   });
@@ -11,16 +12,16 @@ export const getStdDev = (values: Values): number => {
   return Math.sqrt(getMean(squareDiffs));
 };
 
-export const getMean = (values: Values): number => {
-  const sum = values.reduce((sum, value) => sum + value, 0);
+export const getMean = (values: ValuesType): number => {
+  const sum = values.reduce((sum: number, value: number) => sum + value, 0);
   return sum / values.length;
 };
 
-export const getMedian = (values: Values): number => {
+export const getMedian = (values: ValuesType): number => {
   if (values.length === 1) {
     return values[0];
   }
 
-  const numbers = values.sort((a, b) => a - b);
+  const numbers = values.sort((a: number, b: number) => a - b);
   return (numbers[(numbers.length - 1) >> 1] + numbers[numbers.length >> 1]) / 2;
 };
