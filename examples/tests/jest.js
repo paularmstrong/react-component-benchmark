42d15ccd77fab74d7fbaa24011afd8a33d910fb2
Paul Armstrong <paul@spaceyak.com>
2017-09-23 09:47:28 -0700

    Add test example

---
+++ examples/tests/jest.js
@@ -0,0 +1,44 @@
+/* eslint-env jest */
+import Benchmark from 'react-component-bench';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+class Test extends React.Component {
+  render() {
+    return <div id="test" />;
+  }
+}
+
+describe('Benchmark', () => {
+  let props;
+  let meanTime;
+
+  beforeEach(() => {
+    meanTime = 0;
+    props = {
+      component: Test,
+      onComplete: jest.fn(results => {
+        meanTime = results.mean;
+      }),
+      samples: 20
+    };
+  });
+
+  it('mounts in a reasonable amount of time', () => {
+    const component = shallow(<Benchmark {...props} />);
+    component.instance().start();
+    expect(meanTime).toBeLessThan(10);
+  });
+
+  it('updates in a reasonable amount of time', () => {
+    const component = shallow(<Benchmark {...props} type="update" />);
+    component.instance().start();
+    expect(meanTime).toBeLessThan(10);
+  });
+
+  it('unmounts in a reasonable amount of time', () => {
+    const component = shallow(<Benchmark {...props} type="unmount" />);
+    component.instance().start();
+    expect(meanTime).toBeLessThan(10);
+  });
+});
