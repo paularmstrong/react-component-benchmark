1bd1d3da7362e26aa658350e78745893bdc1db88
Paul Armstrong <paul@spaceyak.com>
2017-09-23 09:32:27 -0700

    Add test for result formats

--- src/__tests__/Benchmark.test.js
+++ src/__tests__/Benchmark.test.js
@@ -34,4 +34,43 @@ describe('Benchmark', () => {
       })
     );
   });
+
+  describe('results', () => {
+    let results;
+    beforeAll(() => {
+      const component = shallow(<Benchmark {...props} />);
+      component.instance().start();
+      results = props.onComplete.mock.calls[0][0];
+    });
+
+    Object.entries({
+      startTime: 'number',
+      endTime: 'number',
+      runTime: 'number',
+      sampleCount: 'number',
+      max: 'number',
+      min: 'number',
+      median: 'number',
+      mean: 'number',
+      stdDev: 'number',
+      p70: 'number',
+      p95: 'number',
+      p99: 'number'
+    }).forEach(([key, type]) => {
+      it(`include a ${type} for ${key}`, () => {
+        expect(results).toHaveProperty(key);
+        expect(typeof results[key]).toEqual(type);
+      });
+    });
+
+    it('includes an array for samples', () => {
+      expect(results).toHaveProperty('samples');
+      expect(Array.isArray(results.samples)).toBe(true);
+      results.samples.forEach(sample => {
+        expect(sample).toHaveProperty('start');
+        expect(sample).toHaveProperty('end');
+        expect(sample).toHaveProperty('elapsed');
+      });
+    });
+  });
 });
