28cc301cec3a6b79efd4f76ed3dac1238b8c742d
Paul Armstrong <paul@spaceyak.com>
2021-02-09 15:10:11 -0800

    refactor: update for hooks 🚀

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
+  plugins: ['@babel/plugin-proposal-class-properties'],
+};
--- src/Benchmark.js
+++ src/Benchmark.js
@@ -1,202 +1,223 @@
 // @flow
+import * as React from 'react';
 import * as Timing from './timing';
 import BenchmarkType from './BenchmarkType';
-import React, { Component } from 'react';
 import { getMean, getMedian, getStdDev } from './math';
-
-import type { BenchResultsType, FullSampleTimingType, SampleTimingType } from './types';
+import type { BenchResultsType, FullSampleTimingType } from './types';
 
 const styles = {
   content: {
     position: 'absolute',
-    left: '-999em'
-  }
-};
-
-const emptyObject = {};
-
-const shouldRender = (cycle: number, type: $Values<typeof BenchmarkType>): boolean => {
-  switch (type) {
-    // Render every odd iteration (first, third, etc)
-    // Mounts and unmounts the component
-    case BenchmarkType.MOUNT:
-    case BenchmarkType.UNMOUNT:
-      return !((cycle + 1) % 2);
-    // Render every iteration (updates previously rendered module)
-    case BenchmarkType.UPDATE:
-      return true;
-    default:
-      return false;
-  }
-};
-
-const shouldRecord = (cycle: number, type: $Values<typeof BenchmarkType>): boolean => {
-  switch (type) {
-    // Record every odd iteration (when mounted: first, third, etc)
-    case BenchmarkType.MOUNT:
-      return !((cycle + 1) % 2);
-    // Record every iteration
-    case BenchmarkType.UPDATE:
-      return true;
-    // Record every even iteration (when unmounted)
-    case BenchmarkType.UNMOUNT:
-      return !(cycle % 2);
-    default:
-      return false;
-  }
-};
-
-const isDone = (cycle: number, samples: number, type: $Values<typeof BenchmarkType>): boolean => {
-  switch (type) {
-    case BenchmarkType.MOUNT:
-      return cycle >= samples * 2 - 1;
-    case BenchmarkType.UPDATE:
-      return cycle >= samples - 1;
-    case BenchmarkType.UNMOUNT:
-      return cycle >= samples * 2;
-    default:
-      return true;
-  }
+    left: '-999em',
+  },
 };
 
 const sortNumbers = (a: number, b: number): number => a - b;
 
-type BenchmarkPropsType = {
-  component: typeof React.Component,
+type Props = {|
+  component: React.AbstractComponent<*>,
   componentProps?: {},
   onComplete: (x: BenchResultsType) => void,
   samples: number,
-  timeout: number,
-  type: $Values<typeof BenchmarkType>
-};
+  timeout?: number,
+  type?: $Values<typeof BenchmarkType>,
+|};
 
-type BenchmarkStateType = {
-  componentProps: {},
+type State = {|
+  running: boolean,
+  startTime: number,
   cycle: number,
-  running: boolean
+  samples: Array<FullSampleTimingType>,
+|};
+
+const initialState: State = {
+  running: false,
+  startTime: 0,
+  cycle: 0,
+  samples: [],
 };
 
-/**
- * Benchmark
- * TODO: documentation
- */
-export default class Benchmark extends Component<BenchmarkPropsType, BenchmarkStateType> {
-  _startTime: number;
-  _samples: Array<SampleTimingType>;
+// | 'START_SAMPLE' | 'END_SAMPLE' | 'TICK' | 'END'
+type Action =
+  | {| type: 'START', payload: number |}
+  | {| type: 'START_SAMPLE', payload: number |}
+  | {| type: 'END_SAMPLE', payload: number |}
+  | {| type: 'TICK' |}
+  | {| type: 'END' |};
 
-  static defaultProps = {
-    componentProps: emptyObject,
-    samples: 50,
-    timeout: 10000, // 10 seconds
-    type: BenchmarkType.MOUNT
-  };
+function reducer(state: State = initialState, action: Action) {
+  // console.log(action);
+  switch (action.type) {
+    case 'START':
+      return {
+        ...state,
+        startTime: action.payload,
+        running: true,
+      };
 
-  static Type = BenchmarkType;
-
-  constructor(props: BenchmarkPropsType, context?: {}) {
-    super(props, context);
-    this.state = {
-      componentProps: props.componentProps || emptyObject,
-      cycle: 0,
-      running: false
-    };
-    this._startTime = 0;
-    this._samples = [];
-  }
-
-  componentWillReceiveProps(nextProps: BenchmarkPropsType) {
-    this.setState({ componentProps: nextProps.componentProps });
-  }
-
-  componentWillUpdate(nextProps: BenchmarkPropsType, nextState: BenchmarkStateType) {
-    if (nextState.running && !this.state.running) {
-      this._startTime = Timing.now();
-    }
-  }
-
-  componentDidUpdate() {
-    const { samples, timeout, type } = this.props;
-    const { cycle, running } = this.state;
-
-    if (running && shouldRecord(cycle, type)) {
-      this._samples[cycle].end = Timing.now();
+    case 'START_SAMPLE': {
+      const samples = [...state.samples];
+      samples.push({ start: action.payload, end: -1, elapsed: -1 });
+      return {
+        ...state,
+        samples,
+      };
     }
 
-    if (running) {
-      const now = Timing.now();
-      if (!isDone(cycle, samples, type) && now - this._startTime < timeout) {
-        this._handleCycleComplete();
-      } else {
-        this._handleComplete(now);
-      }
-    }
-  }
-
-  render() {
-    const { component: Component, type } = this.props;
-    const { componentProps, cycle, running } = this.state;
-    if (running && shouldRecord(cycle, type)) {
-      this._samples[cycle] = { start: Timing.now() };
+    case 'END_SAMPLE': {
+      const samples = [...state.samples];
+      const index = samples.length - 1;
+      samples[index].end = action.payload;
+      samples[index].elapsed = action.payload - samples[index].start;
+      return {
+        ...state,
+        samples,
+      };
     }
 
-    return running && shouldRender(cycle, type) ? (
-      <div style={styles.content}>
-        <Component {...componentProps} />
-      </div>
-    ) : null;
-  }
+    case 'TICK':
+      return {
+        ...state,
+        cycle: state.cycle + 1,
+      };
 
-  start() {
-    this._samples = [];
-    this.setState({ running: true, cycle: 0 });
-  }
+    case 'END':
+      return {
+        ...state,
+        running: false,
+      };
 
-  _handleCycleComplete() {
-    const { componentProps, type } = this.props;
-    const { cycle } = this.state;
-
-    // Calculate the component props outside of the time recording (render)
-    // so that it doesn't skew results
-    const nextProps = type === BenchmarkType.UPDATE ? { ...componentProps, 'data-test': cycle } : componentProps;
-
-    this.setState((state: BenchmarkStateType) => ({ componentProps: nextProps, cycle: state.cycle + 1 }));
-  }
-
-  getSamples(): Array<FullSampleTimingType> {
-    return this._samples.reduce((memo: Array<FullSampleTimingType>, { start, end: endTime }: SampleTimingType): Array<
-      FullSampleTimingType
-    > => {
-      const end = endTime || 0;
-      memo.push({ start, end, elapsed: end - start });
-      return memo;
-    }, []);
-  }
-
-  _handleComplete(endTime: number) {
-    const { onComplete } = this.props;
-    const samples = this.getSamples();
-
-    this.setState({ running: false, cycle: 0 });
-
-    const runTime = endTime - this._startTime;
-    const sortedElapsedTimes = samples.map(({ elapsed }: { elapsed: number }): number => elapsed).sort(sortNumbers);
-    const mean = getMean(sortedElapsedTimes);
-    const stdDev = getStdDev(sortedElapsedTimes);
-
-    onComplete({
-      startTime: this._startTime,
-      endTime,
-      runTime,
-      sampleCount: samples.length,
-      samples: samples,
-      max: sortedElapsedTimes[sortedElapsedTimes.length - 1],
-      min: sortedElapsedTimes[0],
-      median: getMedian(sortedElapsedTimes),
-      mean,
-      stdDev,
-      p70: mean + stdDev,
-      p95: mean + stdDev * 2,
-      p99: mean + stdDev * 3
-    });
+    default:
+      return state;
   }
 }
+
+function Benchmark(
+  {
+    component: Component,
+    componentProps,
+    onComplete,
+    samples: numSamples,
+    timeout = 10000,
+    type = BenchmarkType.MOUNT,
+  }: Props,
+  ref: *
+) {
+  const [{ running, cycle, samples, startTime }, dispatch] = React.useReducer(reducer, initialState);
+
+  React.useImperativeHandle(ref, () => ({
+    start: () => {
+      dispatch({ type: 'START', payload: Timing.now() });
+    },
+  }));
+
+  const shouldRender = React.useMemo(() => {
+    switch (type) {
+      // Render every odd iteration (first, third, etc)
+      // Mounts and unmounts the component
+      case BenchmarkType.MOUNT:
+      case BenchmarkType.UNMOUNT:
+        return !((cycle + 1) % 2);
+      // Render every iteration (updates previously rendered module)
+      case BenchmarkType.UPDATE:
+        return true;
+      default:
+        return false;
+    }
+  }, [running, cycle, type]);
+
+  const shouldRecord = React.useMemo(() => {
+    switch (type) {
+      // Record every odd iteration (when mounted: first, third, etc)
+      case BenchmarkType.MOUNT:
+        return !((cycle + 1) % 2);
+      // Record every iteration
+      case BenchmarkType.UPDATE:
+        return true;
+      // Record every even iteration (when unmounted)
+      case BenchmarkType.UNMOUNT:
+        return !(cycle % 2);
+      default:
+        return false;
+    }
+  }, [running, cycle, type]);
+
+  const isDone = React.useMemo(() => {
+    switch (type) {
+      case BenchmarkType.MOUNT:
+      case BenchmarkType.UNMOUNT:
+        return cycle >= numSamples * 2 - 1;
+      case BenchmarkType.UPDATE:
+        return cycle >= numSamples - 1;
+      default:
+        return true;
+    }
+  }, [running, cycle, numSamples, type]);
+
+  React.useMemo(() => {
+    if (running && shouldRecord) {
+      dispatch({ type: 'START_SAMPLE', payload: Timing.now() });
+    }
+  }, [cycle, running, shouldRecord]);
+
+  const handleComplete = React.useCallback(
+    (endTime: number) => {
+      dispatch({ type: 'END' });
+
+      const runTime = endTime - startTime;
+      const sortedElapsedTimes = samples.map(({ elapsed }: { elapsed: number }): number => elapsed).sort(sortNumbers);
+      const mean = getMean(sortedElapsedTimes);
+      const stdDev = getStdDev(sortedElapsedTimes);
+
+      onComplete({
+        startTime,
+        endTime,
+        runTime,
+        sampleCount: samples.length,
+        samples,
+        max: sortedElapsedTimes[sortedElapsedTimes.length - 1],
+        min: sortedElapsedTimes[0],
+        median: getMedian(sortedElapsedTimes),
+        mean,
+        stdDev,
+        p70: mean + stdDev,
+        p95: mean + stdDev * 2,
+        p99: mean + stdDev * 3,
+      });
+    },
+    [onComplete, samples, startTime]
+  );
+
+  React.useEffect(() => {
+    if (!running) {
+      return;
+    }
+
+    const now = Timing.now();
+
+    if (shouldRecord && !samples[samples.length - 1].end) {
+      dispatch({ type: 'END_SAMPLE', payload: now });
+      return;
+    }
+
+    const timedOut = now - startTime > timeout;
+    if (!isDone || timedOut) {
+      setTimeout(() => {
+        dispatch({ type: 'TICK' });
+      }, 1);
+      return;
+    } else if (isDone || timedOut) {
+      handleComplete(now);
+    }
+  }, [running, isDone, samples, shouldRecord, shouldRender, timeout]);
+
+  return (
+    <div style={styles.content}>
+      {running && shouldRender ? <Component {...componentProps} testID={cycle} /> : null}
+    </div>
+  );
+}
+
+export type BenchmarkRef = {| start: () => void |};
+
+export default (React.forwardRef<Props, BenchmarkRef>(Benchmark): React.AbstractComponent<Props, *>);
